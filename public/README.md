9a1cfeb70d613fff16f952a935ed66760917a999
jul4ajka <jul4ajka@gmail.com>
2020-11-09 13:40:34 +0200

    New portfolio on node.js

---
+++ public/README.md
@@ -0,0 +1,5 @@
+# Portfolio
+
+Hello, I'm Julia Chayka website developer! This is all my works and I like to share my experiance.
+
+Have fun!
--- public/js/functions.js
+++ public/js/functions.js
@@ -8,7 +8,9 @@ $(window).scroll(function(){
 
   $('.back-bird').css({
     'transform' : 'translate(0px, '+ wScroll / 4 +'%)'
-  });
+	});
+	
+
 if(wScroll > $('.project-pics').offset().top - ($(window).height() / 1.2)) {
 
     $('.project-pics img').each(function(i){
@@ -19,3 +21,66 @@ if(wScroll > $('.project-pics').offset().top - ($(window).height() / 1.2)) {
     });
   };
 });
+
+var now = new Date();
+var dtString = now.getDate() + "." + now.getMonth() + "." + now.getFullYear();
+document.getElementById("date").textContent = dtString;
+
+// typetext animation
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
